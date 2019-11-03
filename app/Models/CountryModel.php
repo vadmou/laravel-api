253a71a7aab663ab658b26af3f5ed8fd5563d282
Vadym Borovok <vadym.borovok@gmail.com>
2019-11-03 21:36:14 +0200

    add 'select by id' and 'save GET request'

--- app/Models/CountryModel.php
+++ app/Models/CountryModel.php
@@ -7,6 +7,8 @@ use Illuminate\Database\Eloquent\Model;
 class CountryModel extends Model {
     protected $table = "_z_country";
     
+    public $timestamps = false;
+    
     protected $fillable = [
         'iso',
         'name',
