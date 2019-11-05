f0442664ac16c43c417a625a2e4ecd9baf06b07d
Vadym Borovok <vadym.borovok@gmail.com>
2019-11-05 08:32:12 +0200

    add validation rules

--- app/Http/Controllers/Country/CountryController.php
+++ app/Http/Controllers/Country/CountryController.php
@@ -3,9 +3,12 @@
 namespace App\Http\Controllers\Country;
 
 use App\Http\Controllers\Controller;
+use http\Env\Response;
 use Illuminate\Http\Request;
-
 use App\Models\CountryModel;
+
+use Illuminate\Support\Facades\Validator;
+
 
 class CountryController extends Controller
 {
@@ -14,10 +17,22 @@ class CountryController extends Controller
     }
     
     public function countryByID($id){
-        return response()->json(CountryModel::find($id), 200);
+        $country = CountryModel::find($id);
+        if(is_null($country)){
+            return response()->json(["message" => "Record not found!"], 404);
+        }
+        return response()->json($country, 200);
     }
     
     public function countrySave(Request $request){
+        $rules = [
+            'name' => 'required|min:3',
+            'iso' => 'required|min:2|max:2'
+        ];
+        $validator = Validator::make($request->all(), $rules);
+        if($validator->fails()){
+            return response()->json($validator->errors(), 400);
+        }
         $country = CountryModel::create($request->all());
         return response()->json($country, 201);
     }
